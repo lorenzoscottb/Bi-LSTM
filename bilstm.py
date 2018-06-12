29426dd1647353d73a210b92a5891a6b6cad6013
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2018-06-12 10:39:52 +0200

    Update bilstm.py

--- bilstm.py
+++ bilstm.py
@@ -33,49 +33,48 @@ def Bi_LSTM(units, features, time_steps, prn=False):
     return model
 
 
+# select newtwork corpus
+
 # Training
-model = Bi_LSTM(512, 400, length, prn=True)
+model = Bi_LSTM(512, 400, None, prn=True)
 # train model on given % of stimuli
-ts = int((data_length*65)/100)  
-x = list(np.zeros(ts))
-y = list(np.zeros(ts))
+ts = int((len(network_corpus)*65)/100)
 for i in range(ts):
-    x[i] = [vec for vec in network_corpus[i][1]]
-    y[i] = network_corpus[i][0]
+    print('interation '+str(i)+'of'+' '+str(ts))
+    x = [vec for vec in network_corpus[i][1]]
+    y = network_corpus[i][0]
 # reshape: sample, time steps, feature at each time step.
 # if I have 1000 sentences of 10 words, presented in a 3-dim vector:
 # is nb_samples = 1000, time steps =  10, input_dim = 3
-X = array(x).reshape(ts, length, 400)
-Y = array(y).reshape(ts, 1)
-model.fit(X, Y, epochs=50, batch_size=33, verbose=2)
+    length = len(x)
+    X = array(x).reshape(1, length, 400)
+    Y = array(y).reshape(1, 1)
+    model.fit(X, Y, epochs=10, batch_size=33, verbose=2)
 
 # Evaluation
-tt = data_length-ts
-x = list(np.zeros(tt))
-y = list(np.zeros(tt))
-for i in range(tt):
-    x[i] = [vec for vec in network_corpus[i+ts][1]]
-    y[i] = network_corpus[i+ts][0]
-
-X = array(x).reshape(tt, length, 400)
-Y = array(y).reshape(tt, 1)
-yhat = model.predict_classes(X, verbose=2)
+tt = len(network_corpus)-ts
+out = list(np.zeros(tt))
+exp = list(np.zeros(tt))
 correct = 0
 for i in range(tt):
-    exp = y[i]
-    pred = yhat[i]
-    if exp == pred:
+    x = [vec for vec in network_corpus[i+tt][1]]
+    y = network_corpus[i+tt][0]
+    length = len(x)
+    X = array(x).reshape(1, length, 400)
+    Y = array(y).reshape(1, 1)
+    out[i] = model.predict_classes(X, verbose=2)
+    exp[i] = y
+    if y == out[i]:
         correct += 1
-    print('predicted Class: '+str(yhat[i])+' Actual Class: '+
-          str(y[i])+'')
-prediction = list(yhat)
+    print('predicted Class: '+str(out[i])+' Actual Class: '+
+          str(y))
 print('Overall accuracy: '+str(int((correct*100)/tt)))
 
-# plotting model's confusion matrix
-cf = ConfusionMatrix(y, prediction)
+pr = [n for n in nltk.word_tokenize(str(out)) if n.isdigit()]
+# Plotting confusion matrix
+cf = ConfusionMatrix(t, pr)
 cf.plot()
 plt.show()
-
 
 
 
