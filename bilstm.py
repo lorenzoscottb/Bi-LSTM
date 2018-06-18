b59a85b7336db125462c4f59a0bb08385b91fdb0
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2018-06-18 11:37:13 +0100

    Update bilstm.py

--- bilstm.py
+++ bilstm.py
@@ -16,11 +16,7 @@ import matplotlib.pyplot as plt
 
 # The BiLSTM
 def Bi_LSTM(units, features, time_steps, prn=False):
-    # setting the task ; 3D modelling : sample, time steps, and feature.
     model = Sequential()
-    # input shape: (given units, how many time steps)
-    # Bidirectional RNN can concatenate, using merge_mode='concat' !!!
-    # input_shape = (time_steps, features)
     model.add(Bidirectional(LSTM(units, return_sequences=False),
                             input_shape=(time_steps, features),
                             merge_mode='concat'))
@@ -33,20 +29,22 @@ def Bi_LSTM(units, features, time_steps, prn=False):
     return model
 
 
-# select newtwork corpus
-
-# Training
+# Training 
+# setting the task ; 3D modelling : sample, time steps, and feature.
 model = Bi_LSTM(512, 400, None, prn=True)
 # train model on given % of stimuli
+# indicate the newtwork corpus:
+# touples of shape (expected output(integer), sentence(series of vectors))
 ts = int((len(network_corpus)*65)/100)
 for i in range(ts):
     print('interation '+str(i)+'of'+' '+str(ts))
     x = [vec for vec in network_corpus[i][1]]
     y = network_corpus[i][0]
-# reshape: sample, time steps, feature at each time step.
-# if I have 1000 sentences of 10 words, presented in a 3-dim vector:
-# is nb_samples = 1000, time steps =  10, input_dim = 3
+    # extracting each sample (sentence) lenth
     length = len(x)
+    # reshape: sample, time steps, feature at each time step.
+    # if I have 1000 sentences of 10 words, presented in a 3-dim vector:
+    # is nb_samples = 1000, time steps =  10, input_dim = 3
     X = array(x).reshape(1, length, 400)
     Y = array(y).reshape(1, 1)
     model.fit(X, Y, epochs=10, batch_size=33, verbose=2)
